a8bec9ac5fc1db1d074c63682390a23348709fc2
Razat94 <Razat51@gmail.com>
2017-10-24 17:45:52 -0700

    Update maxprotein.hh

--- maxprotein.hh
+++ maxprotein.hh
@@ -200,12 +200,12 @@ std::unique_ptr<FoodVector> filter_food_vector(const FoodVector& source,
 	// Iterate through every item in FoodVector
 	// If there exists a food item that has at least min_kcal kilocalories and at most max_kcal kilocalories, add it to the list.
 	
-	for ( int i = 0; i < source.size() && i < total_size; i++){
-		if (source[i]->kcal() == 0) continue;
-		
+	for ( int i = 0; i < total_size; i++){
+	
 		if (source[i]->kcal() >= min_kcal && source[i]->kcal() <= max_kcal) {
 			result->push_back(source[i]);
 		}
+		
 	}
 
 	return result;
